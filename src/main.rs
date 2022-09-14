bf950d59869e7ead1386f63a8a343f0bcde9c5fe
den-taku <dentaku1412@gmail.com>
2022-09-14 17:45:46 +0900

    Implement simple enumeration

--- src/main.rs
+++ src/main.rs
@@ -18,12 +18,50 @@ fn main() {
         (9, 10),
     ];
 
-    let adjacent = adjacent(vertices, &edges);
+    let adjacent = convert_to_adjacent(vertices, &edges);
 
     println!("{}", is_two_edge_connected(&adjacent));
+
+    let mut ans = Vec::new();
+    for bit in 1..1 << vertices {
+        let mut members = Vec::with_capacity(vertices);
+        for t in 0..vertices {
+            if bit >> t & 1 == 1 {
+                members.push(t)
+            }
+        }
+
+        if members.len() < 2 {
+            continue;
+        }
+
+        let compressed = members
+            .iter()
+            .enumerate()
+            .map(|(i, e)| (e + 1, i + 1))
+            .collect::<std::collections::HashMap<_, _>>();
+
+        let u = members.len();
+        let mut es = Vec::new();
+        for &(u, v) in &edges {
+            if members.contains(&(u - 1)) && members.contains(&(v - 1)) {
+                es.push((*compressed.get(&u).unwrap(), *compressed.get(&v).unwrap()))
+            }
+        }
+        let ads = convert_to_adjacent(u, &es);
+        if is_two_edge_connected(&ads) {
+            print_members(&members);
+            ans.push(members)
+        }
+    }
+    println!("size = {}.", ans.len());
 }
 
-fn adjacent(vertices: usize, edges: &[(usize, usize)]) -> Vec<Vec<usize>> {
+fn print_members(members: &[usize]) {
+    println!("{:?}", members.iter().map(|e| e + 1).collect::<Vec<_>>())
+}
+
+fn convert_to_adjacent(vertices: usize, edges: &[(usize, usize)]) -> Vec<Vec<usize>> {
     let mut adjacent = vec![Vec::new(); vertices];
     for (u, v) in edges {
         adjacent[u - 1].push(v - 1);
@@ -33,19 +71,24 @@ fn adjacent(vertices: usize, edges: &[(usize, usize)]) -> Vec<Vec<usize>> {
 }
 
 fn dfs(
+    before: usize,
     start: usize,
-    rank: usize,
+    rank: &mut usize,
     ord: &mut [Option<usize>],
     low: &mut [usize],
     adjacent: &[Vec<usize>],
 ) {
-    ord[start] = Some(rank);
-    low[start] = rank;
+    ord[start] = Some(*rank);
+    low[start] = *rank;
+    *rank += 1;
     for &neighbor in &adjacent[start] {
         if ord[neighbor].is_none() {
-            dfs(neighbor, rank + 1, ord, low, adjacent);
+            dfs(start, neighbor, rank, ord, low, adjacent);
             low[start] = std::cmp::min(low[start], low[neighbor])
         } else {
+            if neighbor == before {
+                continue;
+            }
             low[start] = std::cmp::min(low[start], ord[neighbor].unwrap())
         }
     }
@@ -54,14 +97,16 @@ fn dfs(
 fn is_two_edge_connected(adjacent: &[Vec<usize>]) -> bool {
     let mut ord = vec![None; adjacent.len()];
     let mut low = vec![std::usize::MAX; adjacent.len()];
-    dfs(0, 0, &mut ord, &mut low, adjacent);
+    let mut rank = 0;
+    dfs(std::usize::MAX, 0, &mut rank, &mut ord, &mut low, adjacent);
 
     if ord.iter().any(|e| e.is_none()) {
         return false;
     }
     let ord = ord.iter().map(|e| e.unwrap()).collect::<Vec<usize>>();
     let mut visited = vec![false; adjacent.len()];
-    have_bridge(0, &ord, &low, adjacent, &mut visited)
+
+    !have_bridge(0, &ord, &low, adjacent, &mut visited)
 }
 
 fn have_bridge(
@@ -74,10 +119,10 @@ fn have_bridge(
     visited[start] = true;
     for &neighbor in &adjacent[start] {
         if !visited[neighbor] {
-            if ord[start] < low[neighbor] || !have_bridge(neighbor, ord, low, adjacent, visited) {
-                return false;
+            if ord[start] < low[neighbor] || have_bridge(neighbor, ord, low, adjacent, visited) {
+                return true;
             }
         }
     }
-    true
+    false
 }
